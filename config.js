5a3512b988790e1b068e4f3d0e3c3a6e8e344d17
JustMattGilbert <matt@everyway.io>
2020-10-27 11:37:33 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Wildvials', // <title>
+  manifestName: 'Wildvials',
+  manifestShortName: 'Wildvials', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Wildvials',
+  subHeading:
+    "Website coming early 2021. Sign up to recieve updates and to be notified when we are live.",
+  socialLinks: [
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://instagram.com/wildvials',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/Wildvials',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/wildvials',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:hello@wildvials.com',
+    },
+  ],
+};
